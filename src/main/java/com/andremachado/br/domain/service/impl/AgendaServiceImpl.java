eae7e53694759895020279fd671bae49bca52fb8
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-10 16:03:09 -0400

    [Added] AgendaService

---
+++ src/main/java/com/andremachado/br/domain/service/impl/AgendaServiceImpl.java
@@ -0,0 +1,7 @@
+package com.andremachado.br.domain.service.impl;
+import com.andremachado.br.domain.service.AgendaService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaServiceImpl implements AgendaService {
+}
