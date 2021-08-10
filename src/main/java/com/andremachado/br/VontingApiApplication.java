e1ac5ed0471b89f0e951490d01c08af7d61fcd1a
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-10 14:55:19 -0400

    first commit

---
+++ src/main/java/com/andremachado/br/VontingApiApplication.java
@@ -0,0 +1,13 @@
+package com.andremachado.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VontingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VontingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andremachado/br/VontingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremachado.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VontingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
