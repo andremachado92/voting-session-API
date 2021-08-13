b8423b2d46173525db9c1a13832057db06a6ce1f
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-13 10:52:24 -0400

    [Added] junit dependency

---
+++ src/test/java/com/andremachado/br/serviceImpl/Agenda.java
@@ -0,0 +1,20 @@
+package com.andremachado.br.serviceImpl;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Agenda {
+
+    @Before
+    private void buildAgenda(){
+
+    }
+
+    @Test
+    private void should_create_new_agenda(){
+
+    }
+
+
+}
